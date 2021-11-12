a3f668016b3bb2d0243f5d560ac3104bfbe29868
randikanonis <randikanonis345@gmail.com>
2021-11-12 14:03:05 +0530

    add validation to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-
 // ES6 Modules or TypeScript
+import Swal from 'sweetalert2';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -18,14 +19,38 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 export class LoginComponent implements OnInit {
 
   login = 'hide';
+  userName = '';
+  password = '';
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.login = 'show';
     }, 1000);
+  }
+
+  submit() {
+    if (this.userName === '' || this.password === '') {
+      Swal.fire({
+        title: 'Info!',
+        text: 'Fields can not be empty',
+        icon: 'info',
+        confirmButtonText: 'Cool'
+      });
+    } else {
+      if (this.userName === 'randika' && this.password === '123') {
+        this.router.navigate(['/game']);
+      } else {
+        Swal.fire({
+          title: 'Error!',
+          text: 'Invalid Credentials',
+          icon: 'error',
+          confirmButtonText: 'Cool'
+        });
+      }
+    }
   }
 
 }
