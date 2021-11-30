e0a7c77c65421ae697fa1eeebcc9b72e7eb6173a
randikanonis <randikanonis345@gmail.com>
2021-11-30 13:43:20 +0530

    add a modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,8 @@ import {IntroComponent} from './intro/intro.component';
 export const routes: Routes = [
   {path: '', redirectTo: '/game', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'intro', component: IntroComponent},
   {path: 'signUp', component: SignUpComponent},
+  {path: 'intro', component: IntroComponent, canActivate: [AuthGuard]},
   {path: 'game', component: GameComponent, canActivate: [AuthGuard]},
 ];
 
